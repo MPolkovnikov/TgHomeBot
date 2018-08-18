5f8567207dd68b8e2b04c2ff33f446084a9386c0
MPolkovnikov <qqw@ya.ru>
2018-08-18 20:17:56 +0300

    Initial commit

---
+++ src/main/java/ru/mpolkovnikov/tghomebot/TghomebotApplication.java
@@ -0,0 +1,12 @@
+package ru.mpolkovnikov.tghomebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TghomebotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TghomebotApplication.class, args);
+    }
+}
